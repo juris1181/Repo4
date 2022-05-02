14fb3496a5ca851bf961b4b0e537875f7aeb5ee8
juris1181 <103937482+juris1181@users.noreply.github.com>
2022-05-02 14:02:02 +0300

    Add files via upload

---
+++ world4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+//#include<conio.h>
+//#include<stdio.h>
+
+int main ()
+
+//hello world izvade
+//hello world izvade
+{
+    string x, y;
+cout<<"Ieraksti savu Vardu: \n";
+cin>>x;
+cout<<"Ieraksti savu Uzvardu: \n";
+cin>>y;
+
+cout<<"Tu ierakstiji: "<<x<<" "<<y<<" !!!";
+
+//return 0;
+}
